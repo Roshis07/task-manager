7a11b532205e084997977349a0d02f06f08005ef
Roshis <roshis.tamang@gmail.com>
2024-09-18 19:35:41 +1000

    basic cleanup & add header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <div class="container">
+        <div class="row">
+          <h1 class="col text-center mt-5">Task Manager</h1>
+        </div>
+      </div>
     </div>
   );
 }
